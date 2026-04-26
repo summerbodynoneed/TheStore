622d530edabf326e2bf7e6cb5295956eec65c6d1
summerbodynoneed <summerbodynoneed@gmail.com>
2026-04-26 08:19:57 +0000

    neufkat

--- product.js
+++ product.js
@@ -4,9 +4,9 @@
 const products = [
   {
     id: 1,
-    name: "Veste coupe-vent",
-    price: 49,
-    attribute: "gilet",
+    name: "Basket",
+    price: 60,
+    attribute: "Vêtement",
     description: "Parfait pour le sport",
     long_description: "Gilet crocheté léger et respirant, idéal pour les activités sportives et les journées fraîches.",
     image_url: "images/ChatGPT Image 25 avr. 2026, 10_48_41.png",
@@ -17,14 +17,62 @@ const products = [
   },
   {
     id: 2,
-    name: "Pantalon sport",
-    price: 39,
-    attribute: "Confort",
-    description: "Léger et respirant",
-    long_description: "Pantalon idéal pour le sport et le quotidien.",
-    image_url: "images/pantalon.jpg",
+    name: "Veste Serpent",
+    price: 50,
+    attribute: "Vêtement",
+    description: "La mue de la veste en jean",
+    long_description: "Veste en jean classique avec une coupe ajustée, parfaite pour un look décontracté.",
+    image_url: "images/IMG_4968.png",
     stock: 5,
     sizes: ["M", "L"],
+    colors: ["Noir"]
+  },
+  {
+    id: 3,
+    name: "Veste Tennis",
+    price: 30,
+    attribute: "Vêtement",
+    description: "Confortable et durable",
+    long_description: "Veste de tennis pré-match, parfaite pour les échauffements et les moments de détente avant le match.",
+    image_url: "images/proto.png",
+    stock: 10,
+    sizes: ["S", "M", "L"],
+    colors: ["Blanc"]
+  },
+  {
+    id: 4,
+    name: "Ensemble SBEUNEUNEUH",
+    price: 40,
+    attribute: "Vêtement",
+    description: "Ensemble pour jogging, quick dry",
+    long_description: "Ensemble confortable et stylé, parfait pour courir et être classe en même temps.",
+    image_url: "images/ensemble.jpeg",
+    stock: 10,
+    sizes: ["S", "M", "L", "XL"],
+    colors: ["Gris"]
+  },
+  {
+    id: 5,
+    name: "Veste sans manches",
+    price: 20,
+    attribute: "Vêtement",
+    description: "Parfaite pour mi-saison",
+    long_description: "Veste imperméable légère inspirée des années 90, mais très moderne.",
+    image_url: "images/veste sm.png",
+    stock: 6,
+    sizes: ["S", "M", "L", "XL"],
+    colors: ["Bleu"]
+  },
+  {
+    id: 6,
+    name: "Chaussures",
+    price: 65,
+    attribute: "Chaussures",
+    description: "Inspirées de l'association sandales + chaussettes",
+    long_description: "Paire de chaussures inspirée du style sandales + chaussettes, offrant un look audacieux et confortable pour les amateurs de mode.",
+    image_url: "images/ChatGPT Image 25. 4. 2026, 20_29_23.png",
+    stock: 50,
+    sizes: ["40", "41", "42", "43", "44"],
     colors: ["Noir"]
   }
 ];
